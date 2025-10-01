7f7f54908a2a7fe3f1f4aca08ad1362551788f01
mon <thanhngan10604@gmail.com>
2025-10-01 14:28:27 +0700

    update detail info

--- components/ProjectResume/index.js
+++ components/ProjectResume/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const ProjectResume = ({ dates, type, position, bullets }) => {
-  const [bulletsLocal, setBulletsLocal] = React.useState(bullets.split(","));
+  // console.log("bullets", bullets);
+  // const [bulletsLocal, setBulletsLocal] = React.useState(bullets.split(","));
+  const [bulletsLocal, setBulletsLocal] = React.useState(bullets);
 
   return (
     <div className="mt-5 w-full flex mob:flex-col desktop:flex-row justify-between">
