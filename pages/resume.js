729319f41d5821d3216110d9d7bd2db785fff153
mon <thanhngan10604@gmail.com>
2025-10-01 20:13:37 +0700

    change title in resume

--- pages/resume.js
+++ pages/resume.js
@@ -54,7 +54,7 @@ const Resume = () => {
                 <Socials />
               </div>
               <div className="mt-5">
-                <h1 className="text-2xl font-bold">Experience</h1>
+                <h1 className="text-2xl font-bold">Projects</h1>
 
                 {resume.experiences.map(
                   ({ id, dates, type, position, bullets }) => (
